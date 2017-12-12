c3df9937a370a510fd7b81885d155b9bc75f44b9
Juan Jimeno <jimenojmm@gmail.com>
2017-12-13 01:13:11 +0800

    omitted tof flight as feedback

--- firmware/arm_child/src/firmware.ino
+++ firmware/arm_child/src/firmware.ino
@@ -1,13 +1,8 @@
 #include <Braccio.h>
 #include <Servo.h>
 
-#include <Wire.h>
-#include <VL53L0X.h>
-
 #define UL_PIN A3
 #define LL_PIN A2
-
-VL53L0X tof_sensor;
 
 Servo base;
 Servo shoulder;
@@ -16,7 +11,6 @@ Servo wrist_roll;
 Servo wrist_pitch;
 Servo gripper;
 
-
 int rec_base, rec_shoulder, rec_elbow, rec_wrist_roll, rec_wrist_pitch, rec_gripper;
 
 void setup() 
@@ -24,49 +18,19 @@ void setup()
     Serial.begin(2000);
     Braccio.begin();
 
-    Wire.begin();
-
-    tof_sensor.init();
-    tof_sensor.setTimeout(500);
-
-    // tof_sensor.setSignalRateLimit(0.1);
-    // tof_sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-    // tof_sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
-    
-    tof_sensor.setMeasurementTimingBudget(200000);
-
-    // tof_sensor.startContinuous();
-
     init_arm();
 }
 
 void loop() 
 {
     static String serial_string = "";
-    static unsigned long prev_height_time = 0;
-    static unsigned long prev_ul_time = 0;
-    static unsigned long prev_ll_time = 0;
+    static unsigned long prev_ls_time = 0;
     
-
-    if((millis() -  prev_height_time) >= 200)
+     if((millis() -  prev_ls_time) >= 200)
     {   
-        Serial.print(get_arm_height());
-        Serial.print('h');
-        prev_height_time = millis();
-    }
-
-     if((millis() -  prev_ul_time) >= 200)
-    {   
-        Serial.print(get_upper_limit());
-        Serial.print('u');
-        prev_ul_time = millis();
-    }
-
-     if((millis() -  prev_ll_time) >= 200)
-    {   
-        Serial.print(get_lower_limit());
+        Serial.print(check_limit_switches());
         Serial.print('l');
-        prev_ll_time = millis();
+        prev_ls_time = millis();
     }
 
     while (Serial.available())
@@ -128,25 +92,16 @@ void init_arm()
     rec_gripper = 0;
 }
 
-double get_arm_height()
+int check_limit_switches()
 {
-    // return tof_sensor.readRangeContinuousMillimeters();
-    double height;
-    int measured_val;
+    if(is_triggered(analogRead(LL_PIN)))
+        return -1;
 
-    measured_val = tof_sensor.readRangeSingleMillimeters();
-    height = (0.0003 * pow((double)measured_val, 2)) + ((0.4082 * (double) measured_val) + 125.9);
-    return height;
-}
+    else if(is_triggered(analogRead(UL_PIN)))
+        return 1;
 
-int get_upper_limit()
-{
-    return is_triggered(analogRead(UL_PIN));
-}
-
-int get_lower_limit()
-{
-    return is_triggered(analogRead(LL_PIN));
+    else    
+        return 0;
 }
 
 float map_float(long x, long in_min, long in_max, long out_min, long out_max)
