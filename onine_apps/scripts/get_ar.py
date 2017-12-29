4b2cb14d20ba086239add6305ec8d29224184cbc
Juan Jimeno <jimenojmm@gmail.com>
2017-12-29 21:25:04 +0800

    added code to calculate yaw manually

--- onine_apps/scripts/get_ar.py
+++ onine_apps/scripts/get_ar.py
@@ -2,27 +2,25 @@
 import rospy, tf
 from geometry_msgs.msg import *
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
+import math
 
 tf_listener = tf.TransformListener() 
-
+     
 if __name__ == '__main__':
     rospy.init_node('get_ar')
 
     while not rospy.is_shutdown():
         try:
-            t = tf_listener.getLatestCommonTime('/torso_link', '/ar_marker_3') 
+            t = tf_listener.getLatestCommonTime('/base_footprint', '/torso_link') 
             if (rospy.Time.now() - t).to_sec() > 0.2:
                 rospy.sleep(0.1)
                 continue
 
-            (item_translation, item_orientation) = tf_listener.lookupTransform('/torso_link', "ar_marker_3", t) 
+            (origin_translation, origin_orientation) = tf_listener.lookupTransform('/base_footprint', "torso_link", t) 
         except(tf.Exception, tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-        (roll,pitch,yaw) = euler_from_quaternion(item_orientation)
 
-        print "ROLL: " + str(roll) 
-        print "PITCH: " + str(pitch) 
-        print "YAW: " + str(-1.570796 + yaw) 
+        # print "ORIGIN TRANSLATION: " + str(origin_translation)
 
         try:
             t = tf_listener.getLatestCommonTime('/base_footprint', '/ar_marker_3') 
@@ -30,9 +28,21 @@ if __name__ == '__main__':
                 rospy.sleep(0.1)
                 continue
 
-            (item_translation, item_orientation) = tf_listener.lookupTransform('/base_footprint', "ar_marker_3", t) 
+            (target_translation, target_orientation) = tf_listener.lookupTransform('/base_footprint', "ar_marker_3", t) 
         except(tf.Exception, tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-        print "AR TRANSLATION: " + str(item_translation)
+        print "TARGET TRANSLATION: " + str(target_translation)
     
+        # distance = calculateDistance(origin_translation[0], origin_translation[1], target_translation[0], target_translation[1])
+        x = target_translation[0] - origin_translation[0]
+        y = target_translation[1] - origin_translation[1]
+
+        print "X: " + str(x)
+        print "Y: " + str(y)
+        if(target_translation[1] > 0):
+            yaw = math.atan(x/y)
+        elif(target_translation[1] < 0):
+            yaw = (math.pi / 2) - math.atan(x/y)
+        print "YAW: " + str(-1 * yaw)
+
         break
